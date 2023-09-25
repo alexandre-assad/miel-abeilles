41b54d76d88e784552f8390b3f88a1f1f0bbd31c
alexandre-assad <alexandre.assad@laplateforme.io>
2023-09-25 14:31:17 +0100

    [ADD] First Init of Classes Hive and Bee

---
+++ bee.py
@@ -0,0 +1,6 @@
+
+
+class Bee:
+
+    def __init__(self):
+        self.genetics = [] #Will be list of flowers traveled through
---
+++ hive.py
@@ -0,0 +1,9 @@
+
+
+class Hive:
+
+
+    def __init__(self):
+        self.pos = [500,500]
+        self.flowers = [] #Will scap flowers_coordinates csv
+        self.bees = [] #will be a list of 100 bees
