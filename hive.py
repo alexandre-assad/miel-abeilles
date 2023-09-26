abf0d36852d4d556c7a9a6b7092120c8ddf1f0fa
alexandre-assad <alexandre.assad@laplateforme.io>
2023-09-26 12:42:07 +0100

    [FIX] Hive Reproduction issue, rewrite

--- hive.py
+++ hive.py
@@ -10,11 +10,7 @@ class Hive:
         self.bees = [Bee(self.flowers) for i in range(100)] #will be a list of 100 bees
 
 
-    def selection_best_travel(self) ->list:
-        return self.bees.sort(key=self.get_score)
 
-    def get_score(self):
-        return self.bees.score
     
     def genetics_algorythm(self):
         """
@@ -22,41 +18,54 @@ class Hive:
         """
         run = True
         while run:
-            self.bees = self.selection_best_travel(self.bees)
+            self.bees.sort(key=self.get_score)
             self.bees = self.bees[:50]
             if self.bees[0].score == self.bees[49].score and self.bees[0].score == self.bees[30].score:
                 run = False
-            for i in range(0,49,2):
-                bees_newborn = self.reproduction(self.bees[0],self.bees[1])
+            for i in range(0,50,2):
+                bees_newborn = self.reproduction(self.bees[i],self.bees[i+1])
                 self.bees.append(bees_newborn[0])
                 self.bees.append(bees_newborn[1])
+            
 
         
     def reproduction(self,bee1:Bee,bee2:Bee) ->list[Bee]:
+        """
+        Input : two bees
+        Basic Code : make a reproduction of two bees, which takes one chromosom by two of parent's ones.
+        Output : a list of two new bees
+        """
         genetic1, genetic2 = bee1.genetics.copy(),bee2.genetics.copy()
         new_born = []
         new_bee1,new_bee2 = Bee(),Bee()
-        while len(genetic1) >0:
-            new_bee1.genetics.append(genetic1[0])
-            genetic2.remove(genetic1[0])
-            genetic1.remove(genetic1[0])
-            if len(genetic2) == 0:
+        while genetic1 != []:
+            gen1 = genetic1[0]
+            new_bee1.genetics.append(gen1)
+            genetic1.remove(gen1)
+            genetic2.remove(gen1)
+            if genetic2 == []:
                 break
-            new_bee1.genetics.append(genetic2[0])
-            genetic1.remove(genetic2[0])
-            genetic2.remove(genetic2[0])
+            gen2 = genetic2[0]
+            new_bee1.genetics.append(gen2)
+            genetic2.remove(gen2)
+            genetic1.remove(gen2)
+        new_bee1.score = new_bee1.fitness_score([500,500])
         new_born.append(new_bee1)
-        genetic1, genetic2 = bee1.genetics.copy(),bee2.genetics.copy()
-        while len(genetic2) >0:
-            new_bee2.genetics.append(genetic2[0])
-            genetic1.remove(genetic2[0])
-            genetic2.remove(genetic2[0])
-            if len(genetic1) == 0:
+        while genetic2 != []:
+            gen1 = genetic2[0]
+            new_bee2.genetics.append(gen1)
+            genetic1.remove(gen1)
+            genetic2.remove(gen1)
+            if genetic1 == []:
                 break
-            new_bee2.genetics.append(genetic1[0])
-            genetic2.remove(genetic1[0])
-            genetic1.remove(genetic1[0])
-            
+            gen2 = genetic1[0]
+            new_bee2.genetics.append(gen2)
+            genetic2.remove(gen2)
+            genetic1.remove(gen2)
+        new_bee2.score = new_bee2.fitness_score([500,500])
         new_born.append(new_bee2)
         return new_born
-            
+
+    def get_score(self,bee):
+        return bee.score
+    
