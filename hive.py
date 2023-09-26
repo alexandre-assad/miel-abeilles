8c417adce7357c3eff46c320b456cdfce16702b2
alexandre-assad <alexandre.assad@laplateforme.io>
2023-09-26 07:45:48 +0100

    [ADD] Complete Init of Hive

--- hive.py
+++ hive.py
@@ -1,9 +1,10 @@
-
+from utils.pandas_utils import coordinates_scrap
+from bee import Bee
 
 class Hive:
 
 
     def __init__(self):
         self.pos = [500,500]
-        self.flowers = [] #Will scap flowers_coordinates csv
-        self.bees = [] #will be a list of 100 bees
+        self.flowers = coordinates_scrap() #Will scap flowers_coordinates csv
+        self.bees = [Bee for i in range(100)] #will be a list of 100 bees
