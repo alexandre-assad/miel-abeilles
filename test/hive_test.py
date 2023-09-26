e80a4ed81c07b36195870497bc8927f5e558812e
alexandre-assad <alexandre.assad@laplateforme.io>
2023-09-26 09:05:33 +0100

    [ADD] Test Hive genetics algorythm

--- test/hive_test.py
+++ test/hive_test.py
@@ -11,3 +11,8 @@ class TestHive(unittest.TestCase):
         self.assertEqual(self.hive1.pos,[500,500])
         self.assertEqual(len(self.hive1.bees),100)
         self.assertEqual(len(self.hive1.flowers),50)
+
+    def test_genetics_algorythm(self):
+        self.hive1.genetics_algorythm()
+        self.assertEqual(len(self.hive1.bees),100)
+        self.assertEqual(self.hive1.bees[2].genetics,self.hive1.bees[31].genetics)
