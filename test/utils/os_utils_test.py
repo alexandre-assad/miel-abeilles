777b56c58d6128a8c9831f91fa5ed231b139cc02
alexandre-assad <alexandre.assad@laplateforme.io>
2023-09-27 08:38:36 +0100

    [ADD] Test Os path graphs

--- test/utils/os_utils_test.py
+++ test/utils/os_utils_test.py
@@ -5,3 +5,6 @@ class TestOsUtils(unittest.TestCase):
 
     def test_path_coordinates(self):
         self.assertEqual(path_coordinates("flowers_coordinates.csv"),"coordinates/flowers_coordinates.csv")
+
+    def test_path_graphs(self):
+        self.assertEqual(path_graphs("average_distance_graph.png","graphs/average_distance_graph.png"))
